065f2fe935ae65468915885a6ef25b8635af487d
VladaStc <vlada.st@yahoo.com>
2022-01-19 12:01:59 +0100

    Initial commit

---
+++ src/main/java/marsrover/Entry.java
@@ -0,0 +1,21 @@
+package marsrover;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static marsrover.InstructionParser.parseGrid;
+import static marsrover.InstructionParser.parsePosition;
+
+public class Entry {
+    public static void main(String[] args) {
+        ArrayList<Rover> rovers = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        Grid grid = parseGrid(scanner.nextLine());
+        while (scanner.hasNext()) {
+            Rover rover = new Rover(grid, parsePosition(scanner.nextLine()));
+            rovers.add(rover);
+            rover.process(scanner.nextLine());
+        }
+        rovers.forEach(r -> System.out.println(r.getPosition()));
+    }
+}
---
+++ src/test/java/marsrover/InstructionParserTest.java
@@ -0,0 +1,20 @@
+package marsrover;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class InstructionParserTest {
+
+    @Test
+    public void testParseGrid() {
+        String instruction = "5 5";
+        assertEquals(new Grid(5, 5), InstructionParser.parseGrid(instruction));
+    }
+
+    @Test
+    public void testParsePosition() {
+        String instruction = "1 2 N";
+        assertEquals(new Position(1, 2, Direction.NORTH), InstructionParser.parsePosition(instruction));
+    }
+}
